8fffcda6dee164a3e29fa52838edaa1948bf96ce
Evan <evan1forjan@outlook.com>
2026-02-28 16:24:19 -0500

    HashLicenseCombined

--- HashLiscense.js
+++ HashLiscense.js
@@ -1,3 +1,4 @@
+
 // Converts list of chars to a list of Ascii values; feeds into HashName
 // Inputs: List
 // Outputs: List
@@ -9,7 +10,7 @@ export function charToAscii(name) {
         };
     console.log(asciiName)
     return(asciiName);
-} 
+};
 
 // Hashes list of ascii values; feeds into CombineLicense
 // Inputs: List
@@ -21,3 +22,32 @@ export function HashName(asciiName) {
     }
     return asciiName
 };
+
+// Takes a list of the hashed drivers license numbers 
+// and returns the same numbers but in a string of numbers
+
+export function CombineList(hashedDL) {
+    let hashedDLstring = "";
+    for (let i = 0; i <hashedDL.length; i++){
+        hashedDLstring = hashedDLstring + hashedDL[i].toString();
+    }
+    return hashedDLstring;
+};
+
+
+//  If the number post-hash does not have the max number of digits,
+// it will fill in 0 until it is at max digits
+
+// This is currently not doing anything, however, if changed to 
+// also take name, it would
+
+export function fillLicense(license){
+    let filler = 24 - license.length;
+    if (license.length < 24){
+        while(license.length < 24){
+            license = license + "0"
+        }
+    }
+    return license;
+};
+
