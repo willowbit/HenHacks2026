140908aa91ccd838ff990ca8ead0f3196b4f9811
Evan <evan1forjan@outlook.com>
2026-02-28 15:43:27 -0500

    FullLiscenceHashing

--- NewOneForWilson.js
+++ NewOneForWilson.js
@@ -2,6 +2,10 @@ import {charToAscii, HashName} from "./HashLiscense.js";
 import{CombineList, fillLicense} from "./CombineList.js";
 import{dlNumber, dobNumber} from "./Wrapper.js";
 
+console.log("charToAscii:" + charToAscii(["0","2","3","4","5","6","7","8"]))
+console.log("HashName:" + HashName(charToAscii(["0","2","3","4","5","6","7","8"])))
+console.log("CombineList:" + CombineList(HashName(charToAscii(["0","2","3","4","5","6","7","8"]))))
+console.log("fillLicense:" + fillLicense(CombineList(HashName(charToAscii(["0","2","3","4","5","6","7","8"])))))
 
 
 console.log("THIS IS DLNUM: " + dlNumber)
