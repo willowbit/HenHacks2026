74b3de68c2ad9d1b3420dada809816b8cd69c016
Evan <evan1forjan@outlook.com>
2026-02-28 16:24:53 -0500

    NewOneUpdated

--- NewOneForWilson.js
+++ NewOneForWilson.js
@@ -1,5 +1,4 @@
-import {charToAscii, HashName} from "./HashLiscense.js";
-import{CombineList, fillLicense} from "./CombineList.js";
+import {charToAscii, HashName,CombineList, fillLicense} from "./HashLiscense.js";
 import{dldob} from "./Wrapper.js";
 
 
@@ -7,6 +6,3 @@ console.log("charToAscii:" + charToAscii(dldob))
 console.log("HashName:" + HashName(charToAscii(dldob)))
 console.log("CombineList:" + CombineList(HashName(charToAscii(dldob))))
 console.log("fillLicense:" + fillLicense(CombineList(HashName(charToAscii(dldob)))))
-
-
-console.log("THIS IS DLNUM: " + dldob)
