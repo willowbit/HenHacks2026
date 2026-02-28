386a14d9da1285beb355d13d27f01f42170baca8
Evan <evan1forjan@outlook.com>
2026-02-28 12:38:21 -0500

    HashName working now?

--- HashLiscense.js
+++ HashLiscense.js
@@ -2,9 +2,14 @@
 
 function HashName(asciiName) {
     for (let i=0; i<asciiName.length; i++){
-    asciiName[i] = (asciiName[i] + 40)/128
+        if (asciiName[i]+40 >= 128) {
+            asciiName[i] = (asciiName[i]+40)/128
+        }else{
+            asciiName[i] = asciiName[i] + 40
+        }
+    
     }
     return asciiName
-}
+};
 
-console.log(HashName([1,1,1,1,1]))
+console.log(HashName([1,2,3,4,5,6,7,8,9]))
