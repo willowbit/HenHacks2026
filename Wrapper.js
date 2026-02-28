9fc07b928fe9d396c2ffc1521c852817e8eb3e04
Willa Magland <liama@tuta.com>
2026-02-28 13:34:26 -0500

    update wrapper + example dl

--- Wrapper.js
+++ Wrapper.js
@@ -1,8 +1,38 @@
 import { createWorker } from 'tesseract.js';
 
-(async () => {
-  const worker = await createWorker('eng');
-  const ret = await worker.recognize('dl.png');
-  console.log(ret.data.text);
+const worker = await createWorker('eng');
+const rectangles = [
+  {
+    left: 320,
+    top: 254,
+    width: 320,
+    height: 240,
+  },
+  {
+    left: 0,
+    top: 630,
+    width: 640,
+    height: 130,
+  },
+];
+
+
+async function gimmevals() {
+    const values = [];
+  for (let i = 0; i < rectangles.length; i++) {
+    const { data: { text } } = await worker.recognize('dl.png', { rectangle: rectangles[i] });
+    values.push(text);
+  }
+  // return values;
+  console.log(values)
   await worker.terminate();
-})();
+}
+
+gimmevals()
+
+// function x() {
+//   let y = gimmevals();
+//   console.log(y);
+// }
+
+// x()
