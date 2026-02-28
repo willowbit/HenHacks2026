d9f4fe173f1631e8ef6d3ca8c86f3038c13901f5
Evan <evan1forjan@outlook.com>
2026-02-28 12:18:18 -0500

    HashName function?

--- HashLiscense.js
+++ HashLiscense.js
@@ -1,1 +1,10 @@
 ///Mel is Mel
+
+function HashName(asciiName) {
+    for (let i=0; i<asciiName.length; i++){
+    asciiName[i] = (asciiName[i] + 40)/128
+    }
+    return asciiName
+}
+
+console.log(HashName([1,1,1,1,1]))
