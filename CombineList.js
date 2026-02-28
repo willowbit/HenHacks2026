12b3300d4ffab5db3c7153541a6c4ac68d2e482f
Evan <evan1forjan@outlook.com>
2026-02-28 15:18:36 -0500

    PENSYLVANIa

--- CombineList.js
+++ CombineList.js
@@ -11,9 +11,9 @@ export function CombineList(hashedDL) {
 console.log(CombineList([23,24,25,25,156,465]));
 
 export function fillLicense(license){
-    let filler = 21 - license.length;
-    if (license.length < 21){
-        while(license.length < 21){
+    let filler = 24 - license.length;
+    if (license.length < 24){
+        while(license.length < 24){
             license = license + "0"
         }
     }
