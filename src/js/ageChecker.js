f84ede0b275307487088a53a2e8836acfeb27c05
Evan <evan1forjan@outlook.com>
2026-03-01 07:40:20 -0500

    ageCheckFunction

---
+++ src/js/ageChecker.js
@@ -0,0 +1,41 @@
+let myBirth = ['1','2','2','5','5','7','8','9','0','3','1','4','2','0','0','7']
+
+const currentYear = Date.now() / 31540000000 + 1970
+
+
+//intakes DlDob list taking advantage of set formatting for age; uses unix current time
+//inputs: list[str]
+//outputs: num
+
+function calculateAge(date){
+    
+    let month = "";
+    let day = "";
+    let year = "";
+    let mob = date.slice(8,10);
+   let dob = date.slice(10,12);
+   let yob = date.slice(12);
+    for (let i = 0; i < mob.length; i++){
+        month += mob[i]
+    }
+    for (let i = 0; i < dob.length; i++){
+        day += dob[i]
+    }
+    for (let i = 0; i < yob.length; i++){
+        year += yob[i]
+}
+    let dateOfBirth = (Number(month)/12) + (Number(day)/365) + Number(year);
+return currentYear - dateOfBirth
+}
+
+function checkAge(age){
+    let oldEnough = false
+    if (age >= 18){
+        oldEnough = true
+    }
+    return oldEnough
+}
+console.log(calculateAge(myBirth));
+
+console.log(checkAge(calculateAge(myBirth)));
+console.log(currentYear);
