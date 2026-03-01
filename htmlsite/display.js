8ae7b21cbb7d677f66dbd73ea3abc443d00d7bbd
wilsonmn <wilsonmn@udel.edu>
2026-02-28 23:27:08 -0500

    html files added

---
+++ htmlsite/display.js
@@ -0,0 +1,4 @@
+const img = document.getElementById("IdImage")
+const savedImage = localStorage.getItem("uploadedId");
+
+img.src = savedImage
---
+++ htmlsite/getimage.js
@@ -0,0 +1,11 @@
+document.getElementById("idform").addEventListener("submit", function(e)
+{e.preventDefault();
+    const file =document.getElementById("idpic").files[0];
+
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        localStorage.setItem("uploadedId", e.target.result);
+        window.location.href = "displayId.html";
+    };
+    reader.readAsDataURL(file)
+})
