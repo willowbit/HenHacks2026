d4fbdcbe737d69dea2e8697c378193e3c4b6e5b1
Willa Magland <liama@tuta.com>
2026-02-28 23:36:03 -0500

    barcodeshit

--- Wrapper.js
+++ Wrapper.js
@@ -1,49 +1,4 @@
-import { createWorker } from 'tesseract.js'; // import tesseract library!
 
-// create tesseract worker
-const worker = await createWorker('eng');
-// create rectangles for DL information
-const rectangles = [
-  {
-    left: 320,
-    top: 254,
-    width: 320,
-    height: 240,
-  },
-  // {
-  //   left: 0,
-  //   top: 2030,
-  //   width: 2230,
-  //   height: 645,
-  // }
-  
-];
-
-// loads image and runs text recognition
-async function getText() {
-    const values = [];
-  for (let i = 0; i < rectangles.length; i++) {
-    const { data: { text } } = await worker.recognize('wilson.jpg', { rectangle: rectangles[i] });
-    values.push(text);
-  }
-  // returns string of values
-  console.log(values)
-  return values;
-  await worker.terminate();
-}
-
-// gets license information
-let license = (await getText())[0].split("\n");
-
-// format the license number as one string
-let licenseNumber = license[0];
-licenseNumber = licenseNumber.replace("4d pLN:", "");
-licenseNumber = licenseNumber.replaceAll(" ", "");
-
-// format the date of birth as one string
-let dateOfBirth = license[1];
-dateOfBirth = dateOfBirth.replace("Q poe:", "");
-dateOfBirth = dateOfBirth.replaceAll("/", "");
 
 // concatenate the license and date of birth strings as one string
 let ddAndDob = licenseNumber + dateOfBirth;
---
+++ readBarcode.py
@@ -0,0 +1,16 @@
+from PIL import Image as PIL
+from pdf417decoder import PDF417Decoder
+
+image = PIL.open("./wilson.png")
+decoder = PDF417Decoder(image)
+
+if (decoder.decode() > 0):
+    decoded = decoder.barcode_data_index_to_string(0)
+
+stuff = decoded.split("\n")
+
+DLN = stuff[1].split("DLDAQ")[-1]
+DOB = stuff[12].split("DBB")[1]
+
+NumAndDOB = str(DLN) + str(DOB)
+print(NumAndDOB)
